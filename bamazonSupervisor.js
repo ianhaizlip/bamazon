92e4571dc3138762e37ee2302067c02a9fbc3d35
Ian Haizlip <ianhaizlip@Ians-MacBook-Pro.local>
2018-02-12 17:54:10 -0700

    11 feb 2018

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,5 @@
 // Require
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
 //mySQL Connection
@@ -25,8 +25,8 @@ function supervisor(){
 	inquirer.prompt(
 		//customer input
 		[{
-	      	name: "list",
-	      	type: "supervisor",
+	      	name: "supervisor",
+	      	type: "list",
 	      	message: "What would you like to do?",
 	      	choices: ["View product sales by department", "Create New Department"]
 	    }])
@@ -35,13 +35,11 @@ function supervisor(){
       	console.log('answer:', answer.supervisor);
 	    if(answer.supervisor == 'View product sales by department'){
 	    	//show supervisor table
-	      	connection.query(`SELECT department_id, department_name, overhead_cost, products.product_sales,
-	      		SUM(IF(departments.department_name = products.department_name), products.product_sales ,AS product_sales),
-				SUM(product_sales) - over_head_costs) AS total_profit,
-	      		FROM departments 
-	      		JOIN LEFT products 
-	      		ON (departments.department_name = products.department_name)
-	      		GROUP BY department_id`,
+	      	connection.query(`SELECT departments.department_id, departments.department_name,
+	      	  	departments.overhead_costs, SUM(product_sales),
+	      	  	(SUM(product_sales) - over_head_costs) AS total_profit FROM departments 
+	      	  	INNER JOIN products ON (departments.department_name = products.department_name) 
+	      	  	GROUP BY department_id`,
 	      	 function(err, result) {
 		    	if (err) {
 		    		console.log('get stock error:\n',err);
@@ -55,17 +53,29 @@ function supervisor(){
 		if(answer.supervisor == 'Create New Department'){
 			inquirer.prompt(
 			[{
-				name: "input",
-		      	type: "department_name",
+				name: "department_name",
+		      	type: "input",
 		      	message: "What is the name of the new department?"
 		    },
 		    {
-				name: "input",
-		      	type: "overhead_cost",
+				name: "overhead_cost",
+		      	type: "input",
 		      	message: "What is the over head of the new department?"
-	      	}]).then(
-	      		connection.query(`INSERT INTO (department_name, overhead_cost) VALUES ?`,
-	      		[answer], 
+	      	}]).then(function (answer){
+
+	      		insertDepartment(answer);
+	      		console.log('New department created.');
+	      		
+	      	});
+			
+		}
+    });
+
+
+}
+
+function insertDepartment(input){
+	connection.query(`INSERT INTO departments (department_name, overhead_cost) VALUES ?`, [input],
 	      		function(err, result) {
 				    if (err) {
 				    	console.log('New department error:\n',err);
@@ -75,10 +85,4 @@ function supervisor(){
 				    }
 
 				});
-	      	);
-			
-		}
-    });
-
-
 }
